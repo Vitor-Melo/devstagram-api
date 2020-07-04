94752833239e49f2c5df8e6aed7e3b63dc711fa8
Vitor-Melo <vitorpmelo20@gmail.com>
2020-07-03 21:38:44 -0300

    Login - Classe Users, Controller Users criado

--- Controllers/UsersController.php
+++ Controllers/UsersController.php
@@ -2,8 +2,35 @@
 namespace Controllers;
 
 use \Core\Controller;
-use \Models\Usuarios;
+use \Models\users;
 
 class UsersController extends Controller {
-    
+
+    public function index(){}
+
+    public function login()
+    {
+        $array = array('error'=>'');
+
+        $method = $this->getMethod();
+        $data = $this->getRequestData();
+
+        if($method == 'POST'){
+            if(!empty($data['email']) && !empty($data['pass'])){
+                $user = new Users;
+                if ($user->checkCredentials($data['email'], $data['pass'])){
+                    $array['error'] = 'Tudo certo!';
+                } else {
+                    $array['error'] = 'Acesso negado!';
+                }
+                
+            }
+            
+        } else {
+            $array['error'] = 'Método requisição incompátivel';
+        }
+
+        $this->returnJson($array);
+    }
+
 }
---
+++ Models/Users.php
@@ -0,0 +1,30 @@
+<?php
+namespace Models;
+
+use Core\Model;
+
+class Users extends Model {
+
+    private $id_user;
+
+    public function checkCredentials($email, $pass){
+
+        $sql = 'SELECT pass, id FROM users WHERE email = :email';
+        $sql = $this->db->prepare($sql);
+        $sql->bindValue(':email', $email);
+        $sql->execute();
+
+        if ($sql->rowCount() > 0)
+        {
+            $info = $sql->fetch();
+
+            if (password_verify($pass, $info['pass'])){
+                $this->id_user = $info['id'];
+                return true;
+            } else {
+                return false;
+            }
+            return false;
+        }
+    }
+}
