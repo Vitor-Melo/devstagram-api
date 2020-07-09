735dba13badcebcb039c3e32639a75a01b92f43c
Vitor-Melo <vitorpmelo20@gmail.com>
2020-07-09 20:25:56 -0300

    Endpoint users/id

--- Controllers/UsersController.php
+++ Controllers/UsersController.php
@@ -88,6 +88,13 @@ class UsersController extends Controller {
 
             switch($method){
                 case 'GET':
+                    $array['data'] = $users->getInfo($id);
+
+                    if (count($array['data']) === 0)
+                    {
+                        $array['error'] = 'Usuário não existe';
+                    }
+
                     break;
                 case 'PUT':
                     break;
